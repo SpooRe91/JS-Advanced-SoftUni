76e10c770e90bcb41289008a069f9fedcceb8845
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-12 15:42:59 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS-Advanced/JS-Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        let text = document.getElementById("searchField").value.toLowerCase();
+        let elements = Array.from(document.querySelectorAll("tbody tr"));
+        elements.forEach((el) => {
+            if (el.textContent.toLowerCase().includes(text)) {
+                el.classList.add("select");
+            } else {
+                el.classList.remove("select");
+            }
 
-   }
+        })
+        document.getElementById("searchField").value = "";
+
+    }
+
 }
