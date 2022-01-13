acb4c1e056d84e0c8b8217c1807545eaf5d52937
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-13 15:50:29 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS-Advanced/JS-Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,20 @@
 function generateReport() {
-    //TODO
+    let checkboxes = document.querySelectorAll('table thead tr th input'); //list with all checkboxes
+    let rows = document.querySelectorAll('tbody tr');   //list with all rows
+    let output = [];
+    for (let i = 0; i < rows.length; i++) {
+        let obj = {};
+
+        //get the textContent of every row
+        let values = Array.from(rows[i].getElementsByTagName('td')).map(el => el.textContent);
+        
+        //see if it is checked and get all the values of the row
+        for (let j = 0; j < checkboxes.length; j++) {
+            if (checkboxes[j].checked) {
+                obj[checkboxes[j].name] = values[j];
+            }
+        }
+        output.push(obj);
+    }
+    document.querySelector('#output').value = JSON.stringify(output);
 }
