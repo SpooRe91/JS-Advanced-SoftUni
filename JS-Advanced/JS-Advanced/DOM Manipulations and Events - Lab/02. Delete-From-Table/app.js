e3825281730e280a5e5a7c21757120b54af410fa
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-14 14:00:34 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
+++ JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
@@ -1,30 +1,9 @@
-function deleteByEmail() {
-
-    let input = document.querySelector("label input").value;
-    let rows = Array.from(document.querySelectorAll('tbody tr'));
-    //console.log(rows[1].querySelectorAll('td')[1].textContent);
-
-    rows.forEach((row)=>{
-        let email = row.querySelectorAll('td')[1].textContent;
-        if(email==input){
-            row.parentNode.removeChild(row);
-            document.getElementById('result').textContent = "Deleted."
-        }else{
-            document.getElementById('result').textContent = "Not found."
-
-        }
-    })
-
-}
-
-
-
 // function deleteByEmail() {
 //     let email = document.getElementsByName("email")[0].value;
 //     let secondColumn = document.querySelectorAll(
 //       "#customers tr td:nth-child(2)");
 //     for (let td of secondColumn)
-//       if (td.textContent == email) {zx
+//       if (td.textContent == email) {
 //         let row = td.parentNode;
 //         row.parentNode.removeChild(row);
 //         document.getElementById('result').
@@ -34,3 +13,26 @@ function deleteByEmail() {
 //     document.getElementById('result').textContent = "Not found.";
 //   }
   
+
+
+
+  function deleteByEmail() {
+
+    let input = document.querySelector("label input").value;
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    
+
+    for (let row of rows) {
+        let email = row.querySelectorAll('td')[1].textContent;
+        if(email==input){
+            row.parentNode.removeChild(row);
+            document.getElementById('result').textContent = "Deleted."
+            return;
+        }else{
+            document.getElementById('result').textContent = "Not found."
+
+        }
+    }
+
+}
+
