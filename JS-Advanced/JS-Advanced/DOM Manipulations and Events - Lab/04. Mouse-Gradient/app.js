2ca8ba543f26afc6259d1f329c068f47551613b5
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-14 14:34:37 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
+++ JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,20 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+
+    let gradient = document.getElementById('gradient');
+
+    gradient.addEventListener('mouseout', mouseOut);
+
+    gradient.addEventListener('mousemove', mouseMove);
+
+    function mouseMove(ev) {
+        let power = ev.offsetX / (ev.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";
+
+    }
+
+    function mouseOut(ev) {
+        document.getElementById('result').textContent = "";
+
+    }
 }
