d02f77450de856011cfb64fb0fdaaa305d4699f4
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-14 14:00:13 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++ JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
@@ -9,3 +9,14 @@ function addItem() {
     input.value ='';
 
 }
+
+
+// function addItem() {
+//     let text = document.getElementById('newItemText').value;
+//     let li = document.createElement("li");
+//     li.appendChild(document.createTextNode(text));
+//     document.getElementById("items").appendChild(li);
+//       //clearing the input:
+//     document.getElementById('newItemText').value = '';
+//   } 
+  
--- JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
+++ JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,36 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    let input = document.querySelector("label input").value;
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    //console.log(rows[1].querySelectorAll('td')[1].textContent);
+
+    rows.forEach((row)=>{
+        let email = row.querySelectorAll('td')[1].textContent;
+        if(email==input){
+            row.parentNode.removeChild(row);
+            document.getElementById('result').textContent = "Deleted."
+        }else{
+            document.getElementById('result').textContent = "Not found."
+
+        }
+    })
+
 }
+
+
+
+// function deleteByEmail() {
+//     let email = document.getElementsByName("email")[0].value;
+//     let secondColumn = document.querySelectorAll(
+//       "#customers tr td:nth-child(2)");
+//     for (let td of secondColumn)
+//       if (td.textContent == email) {zx
+//         let row = td.parentNode;
+//         row.parentNode.removeChild(row);
+//         document.getElementById('result').
+//           textContent = "Deleted.";
+//         return;
+//       }
+//     document.getElementById('result').textContent = "Not found.";
+//   }
+  
