9b0f4c223b7caf6cae0df7301c1ff6d56695ff99
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-14 15:18:13 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/06. Dynamic-Validation/app.js
+++ JS-Advanced/JS-Advanced/DOM Manipulations and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+
+    let regExp = /([a-z]{1,})@([a-z]{1,})\.([a-z]{1,})/gm;
+
+    let input = document.getElementById('email');
+
+    input.addEventListener('change',checkValidity);
+
+    function checkValidity(ev) {
+        if(!ev.target.value.match(regExp)) {
+            ev.target.classList.add('error');
+        }else{
+            ev.target.classList.remove('error');
+        }
+    }
+
+
 }
