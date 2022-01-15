c0066a6a3f460806203f03169f1c40c8205249b7
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-15 14:47:23 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS-Advanced/JS-Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,17 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    document.getElementById('main').addEventListener('click', showHideProfile)
+
+    function showHideProfile(ev) {
+        if(ev.target.parentNode.querySelector('input[type="radio"]').checked==false) {
+            if (ev.target.textContent == 'Show more') {
+                ev.target.parentNode.querySelector('div').style.display = "block"
+                ev.target.textContent = 'Hide it'
+            }else if (ev.target.textContent == 'Hide it') {
+                ev.target.parentNode.querySelector('div').style.display = "none"
+                ev.target.textContent = 'Show more';
+            }
+        }
+        
+    }
 }
