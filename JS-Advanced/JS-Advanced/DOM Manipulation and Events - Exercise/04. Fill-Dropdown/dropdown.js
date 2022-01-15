6ed6d440b9744446e6f1d45a686285ef11a15f5d
Iliyan Rusev <ilianrusev@gmail.com>
2022-01-15 15:13:11 +0200

    Add files via upload

--- JS-Advanced/JS-Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/JS-Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let inputText = document.getElementById('newItemText');
+    let inputValue = document.getElementById('newItemValue');
+
+    let newOption = document.createElement('OPTION');
+    newOption.textContent = inputText.value;
+    newOption.value = inputValue.value;
+    document.getElementById('menu').appendChild(newOption);
+
+    inputText.value = '';
+    inputValue.value = '';
 }
